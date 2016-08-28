a6e49855f0133458da3a4a137c380c1495d2d6ca
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-08-28 10:20:19 -0300

    Add planning stuff

--- hack-nu/db/seeds.rb
+++ hack-nu/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Planning.create([{category:'Gastos fixos', subcategory: 'conta água', value: 30},
+Planning.create([{category:'Gastos fixos', subcategory: 'Conta água', value: 30},
   {category:'Gastos fixos', subcategory: 'Conta luz', value: 50},
   {category:'Gastos fixos', subcategory: 'Aluguel', value: 2000},
   {category:'Gastos fixos', subcategory: 'Telefonia', value: 90},
@@ -16,3 +16,11 @@ Planning.create([{category:'Gastos fixos', subcategory: 'conta água', value: 30
   {category:'Gastos variáveis', subcategory: 'Serviços', value: 20},
   {category:'Gastos variáveis', subcategory: 'Transporte', value: 80},
   {category:'Gastos variáveis', subcategory: 'Viagem', value: 500}])
+
+AccountDatum.create([{amount: 100, category: 'Gastos variáveis', name: 'Bares e Restaurantes'},
+  {amount: 50, category: 'Gastos variáveis', name: 'Educação'},
+  {amount: 20, category: 'Gastos variáveis', name: 'Saúde'},
+  {amount: 900, category: 'Gastos variáveis', name: 'Viagem'},
+  {amount: 20, category: 'Gastos fixos', name: 'Conta água'},
+  {amount: 50, category: 'Gastos fixos', name: 'Conta luz'},
+  {amount: 2000, category: 'Gastos fixos', name: 'Aluguel'}])
